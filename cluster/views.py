594106dddb89d242cae3a72fec2f4b4f8e85f129
ksh322 <stevekim97@naver.com>
2022-11-28 17:32:14 +0900

    add cluster update

--- cluster/views.py
+++ cluster/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Cluster
+from django.utils import timezone
 
 # Create your views here.
 def addcluster(request):
     print("hello")
+    if(request.method == "POST"):
+        cluster = Cluster()
+        cluster.cluster_name = request.POST['clustername']
+        cluster.cluster_type = request.POST['typeradio']
+        cluster.bearer_token = request.POST['bearertoken']
+        cluster.kubeconfig = request.POST['kubecfg']
+        cluster.save()
+    
     return render(request, 'addcluster.html')

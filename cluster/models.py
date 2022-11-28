4e8f66b68eeb7ef06e4ff2e3b925a104295cefea
ksh322 <stevekim97@naver.com>
2022-11-28 16:20:35 +0900

    list_update

--- cluster/models.py
+++ cluster/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class cluster(models.Model):
     cluster_name = models.CharField(max_length=100, primary_key=True)
     cluster_type = models.DateTimeField(auto_now_add=True)
-    bearer_token = models.TextField()
     kubeconfig = models.CharField(max_length=100)
     cluster_url = models.CharField(max_length=100)
     user_id = models.CharField(max_length=100)
     insert_date = models.CharField(max_length=100)
+    bearer_token = models.TextField()

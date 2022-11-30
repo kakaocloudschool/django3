4f4678bb52e1f018c0d698ceeecc93b41e466581
ksh322 <stevekim97@naver.com>
2022-11-30 16:59:06 +0900

    user model add privilege

--- member/forms.py
+++ member/forms.py
@@ -1,11 +1,7 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import CustomUser
 
-
-class UserForm(UserCreationForm):
-    email = forms.EmailField(label="이메일")
-
-    class Meta:
-        model = User
-        fields = ("username", "password1", "password2", "email")
+class SignupForm(UserCreationForm):
+  class Meta:
+    model = CustomUser
+    fields = ['username', 'password1', 'password2', 'email', 'group', 'privilege']
--- member/views.py
+++ member/views.py
@@ -1,19 +1,17 @@
-from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from member.forms import UserForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib import auth
+from .forms import SignupForm
 
+def signup_view(request):
+  if request.method == 'POST':
+    form = SignupForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      auth.login(request, user)
+      return redirect('login')
+    return redirect('login')
 
-def signup(request):
-    if request.method == "POST":
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            
-            user = authenticate(username=username, password=raw_password)  # 사용자 인증
-            login(request, user)  # 로그인
-            return redirect('login')
-    else:
-        form = UserForm()
-    return render(request, 'member/signup.html', {'form': form})
+  else:
+    form = SignupForm()
+    return render(request, 'member/signup.html', {'form' : form})
